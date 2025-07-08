2844655935acdd69fd73a42db2bb846c72f7a93a
LitterBanana <jusufherlamb@gmail.com>
2025-07-08 20:39:20 +0700

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-      
+      <div className="container">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ipsum saepe assumenda consequatur ratione, fuga eum. Repellendus earum et placeat exercitationem quasi aliquam. Necessitatibus debitis reiciendis id ea nihil libero. Eius quod consectetur libero commodi quis et cum est, debitis pariatur totam non dicta vitae minus itaque, reiciendis eligendi eum adipisci quia. Expedita tempore at asperiores ea facere quas dolores repellendus iure, quisquam minima ipsam debitis minus earum accusamus, voluptatem, eius doloremque error nihil non cum laudantium neque suscipit aliquam. Quo officia, eius nostrum debitis repellat ut sit velit tempora unde necessitatibus, voluptatibus, iure ullam modi dolor? Consequuntur, aperiam qui.</p>
+      </div>
   );
 }
